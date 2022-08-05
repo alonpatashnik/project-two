9bd271b269c0a1a2fe9f19663fe928deb30dc20f
RainierDotulong <rainierkd@gmail.com>
2022-08-05 12:53:28 -0700

    added register logic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require('./config/connection')
 // const { Trail, User, Playlist } = require('./models')
 const exphbs = require('express-handlebars')
 // const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const bcrypt = require('bcrypt')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -18,8 +19,31 @@ const hbs = exphbs.create({})
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
+const users = []
+
+app.get('/login', (req, res) => {
 	res.render('login')
+})
+app.get('/register', (req, res) => {
+	res.render('register')
+})
+app.post('/register', async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		users.push({
+			name: req.body.username,
+			email: req.body.email,
+			password: hashedPassword,
+		})
+		res.redirect('/login')
+		//store users in database
+	} catch {
+		//reload if error
+		console.log('there was an error in creating account')
+		res.redirect('/register')
+	}
+	console.log(users)
+	res.render('register')
 })
 // turn on routes
 // app.use(routes);
