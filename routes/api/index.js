6895e0820010abd5a8300e75c8205659e93bc95b
alonpatashnik <alonp714@gmail.com>
2022-08-05 13:31:55 -0700

    minor associations

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
-const playlistRoutes = require('./playlistRoutes');
 const userRoutes = require('./userRoutes');
-const trailRoutes = require('./trailRoutes');
 
-router.use('/playlist', playlistRoutes);
 router.use('/user', userRoutes);
-router.use('/trail', trailRoutes);
 
 module.exports = router;
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const { User, Trail, Playlist } = require('../../models');
 
 router.get('/', async (req, res) => {
-    
+
 })
 
 router.post('/', async (req, res) => {
     
 })
+
+module.exports = router
