cb6629f838b61ccdcb4cbdcf857c84b5016f200c
alonpatashnik <106717239+alonpatashnik@users.noreply.github.com>
2022-08-08 16:55:50 -0700

    Revert "associations added"

--- seeds/playlistdata.js
+++ seeds/playlistdata.js
@@ -5,25 +5,29 @@ const playlistData = [
         "playlist_title": "Wrestling with Adjectives",
         "tags": "peaceful",
         "upvotes": 35,
-        "playlist_link": "https://open.spotify.com/playlist/0FCgn1i2TvoafMG0rRf6iI?si=4fd2298800414793"
+        "playlist_link": "https://open.spotify.com/playlist/0FCgn1i2TvoafMG0rRf6iI?si=4fd2298800414793",
+        "author_id": 1
     },
     {
         "playlist_title": "Beats to Relax to",
         "tags": "focus",
         "upvotes": 51,
-        "playlist_link": "https://youtu.be/jfKfPfyJRdk"
+        "playlist_link": "https://youtu.be/jfKfPfyJRdk",
+        "author_id": 4
     },
     {
         "playlist_title": "Pokemon Cities",
         "tags": "upbeat",
         "upvotes": 131,
-        "playlist_link": "https://open.spotify.com/playlist/4PRZumvhyoOQNyo98Sj8ih?si=28c55ccc662d4cdb"
+        "playlist_link": "https://open.spotify.com/playlist/4PRZumvhyoOQNyo98Sj8ih?si=28c55ccc662d4cdb",
+        "author_id": 4
     },
     {
         "playlist_title": "Ive Got My Mind Set On You",
         "tags": "happy",
         "upvotes": 87,
-        "playlist_link": "https://open.spotify.com/playlist/37i9dQZF1E8PKwGoSUThTG?si=536bbe84d1bd4227"
+        "playlist_link": "https://open.spotify.com/playlist/37i9dQZF1E8PKwGoSUThTG?si=536bbe84d1bd4227",
+        "author_id": 2
     }
 
 ]
--- server.js
+++ server.js
@@ -126,6 +126,6 @@ app.post('/login', async (req, res) => {
 app.use('/', routes)
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log('Now listening ' + PORT))
 })
