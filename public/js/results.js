52fe4d8ff0d06f78fa24e3c909faf2b98413ee80
RainierDotulong <rainierkd@gmail.com>
2022-08-09 16:00:47 -0700

    fixed the login register and result search bar

--- public/js/results.js
+++ public/js/results.js
@@ -1,11 +1,10 @@
-
-document.querySelector('#resultSearchForm').addEventListener("submit", (e) => {
-    e.preventDefault();
-    const searchTerm = document.querySelector('#resultSearchBar').value
-    console.log(searchTerm)
-    if (searchTerm) {
-        document.location.replace('/' + searchTerm)
-    } else {
-        console.log("ooops")
-    }
+document.querySelector('#resultSearchForm').addEventListener('submit', (e) => {
+	e.preventDefault()
+	const searchTerm = document.querySelector('#resultSearchBar').value
+	console.log(searchTerm)
+	if (searchTerm) {
+		document.location.replace('/result/' + searchTerm)
+	} else {
+		console.log('ooops')
+	}
 })
